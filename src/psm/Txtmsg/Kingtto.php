faed0e3229bd078a3ea04396baf374499956c53c
shuhai <admin@4wei.cn>
2017-03-11 17:29:32 +0800

    优化短信发送

--- src/psm/Txtmsg/Kingtto.php
+++ src/psm/Txtmsg/Kingtto.php
@@ -7,14 +7,15 @@ class Kingtto extends Core
     public function sendSMS($message)
     {
         $p["action"]   = "send";
-        $p["userid"]   = 4425;
+        $p["userid"]   = $this->originator;
         $p["account"]  = $this->username;
         $p["password"] = $this->password;
-        $p["mobile"]   = join(",", $phone);
+        $p["mobile"]   = join(",", $this->recipients);
         $p["content"]  = sprintf("【海南坚果】告警业务:%s，告警内容:%s", "CloudMonitor", $message);
         $parameter = http_build_query($p);
         $url = "http://sms.kingtto.com:9999/sms.aspx?{$parameter}";
-        $return =  `curl -s '$url'`;
+        $return = `curl -s '$url'`;
+
         $return = simplexml_load_string($return);
         return $return->returnstatus == "Success";
     }
